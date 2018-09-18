3540cdca7908d157f08908fb56fa55bf9b357a32
Emily Papale <emilypapale1@gmail.com>
2018-09-18 02:14:54 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,19 @@
+def introduction
+end
+def introduction(name)
+end
+def introduction(name)
+  puts "Hi, my name is #
+  {name}."
+end
+def introduction(name)
+  puts "Hi my name is #
+  {name} and I am learning to program in #
+  {language}."
+end
+def introduction(name)
+  puts "Hi, my name is # {name}"
+  puts "and I am learning to program in # {language}"
+  def introduction(name)(language)
+end
+
