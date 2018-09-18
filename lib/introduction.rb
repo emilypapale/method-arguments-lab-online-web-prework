bd2d743dc01ace7ebbb94d533e6d883635f30a12
Emily Papale <emilypapale1@gmail.com>
2018-09-18 02:13:11 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,3 +11,7 @@ def introduction(name)
   {name} and I am learning to program in #
   {language}."
 end
+def introduction(name)
+  puts "Hi, my name is # {name}"
+  puts "and I am learning to program in # {language}"
+end
